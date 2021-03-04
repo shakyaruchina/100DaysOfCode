7756d0758f2f3ff8bce7bd0dc10c0981d96b47a7
Ruchina Shakya <ruchinas@gmail.com>
2021-03-04 13:21:57 -0700

    python functions

---
+++ daySix/pyFunctions.py
@@ -0,0 +1,12 @@
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+#own function
+#defining custom function
+def my_function():
+    print("Hello")
+    print("Bye")
+#call/print function
+
+my_function()
